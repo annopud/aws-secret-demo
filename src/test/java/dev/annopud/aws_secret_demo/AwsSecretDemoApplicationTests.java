7ba53f466b0438fd1f1e0e913c9b2c4c7410c67e
tttmb <annop.email@gmail.com>
2025-04-03 23:32:59 +0700

    first commit

---
+++ src/test/java/dev/annopud/aws_secret_demo/AwsSecretDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.annopud.aws_secret_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSecretDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
